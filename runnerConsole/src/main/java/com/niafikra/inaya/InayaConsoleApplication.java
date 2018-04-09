3136833fec574390279d8c192d449e71e4ef27fb
mbwana.mbura <mbwanajm@gmail.com>
2018-04-09 17:31:55 +0300

    initial commit

---
+++ runnerConsole/src/main/java/com/niafikra/inaya/InayaConsoleApplication.java
@@ -0,0 +1,11 @@
+package com.niafikra.inaya;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InayaConsoleApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(InayaConsoleApplication.class, args);
+    }
+}
---
+++ runnerVaadin/src/main/java/com/niafikra/inaya/InayaServletInitializer.java
@@ -0,0 +1,24 @@
+package com.niafikra.inaya;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+/**
+ * The entry point of the Spring Boot application.
+ */
+@SpringBootApplication(scanBasePackages = "com.niafikra.inaya")
+public class InayaServletInitializer extends SpringBootServletInitializer {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InayaServletInitializer.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(
+            SpringApplicationBuilder builder) {
+        return builder.sources(InayaServletInitializer.class);
+    }
+
+}
